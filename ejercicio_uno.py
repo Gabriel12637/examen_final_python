99255d858f49c53734f597949c9876109249b2a3
hackanonimous <gabriel12637>
2024-07-17 09:39:35 -0500

    solucion: <uno> ejercicio

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,13 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_menor_y_mayor(lista):
+    if not lista:
+        return None
+    
+    return {'menor': min(lista), 'mayor': max(lista)}
+
+# Ejemplo de uso:
+entrada = [4, 7, 10, 4, 1, 0]
+resultado = encontrar_menor_y_mayor(entrada)
+print(resultado)  # Output: {'menor': 0, 'mayor': 10}
